b564ae164ab238021ac523fce2202469e9c176b8
Эльмира <capricorn_26@mail.ru>
2020-06-22 01:27:11 +0700

    Добавляет эффекты и масштабирование изображения

--- js/upload.js
+++ js/upload.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var uploadButton = document.querySelector('#upload-file');
 var upload = document.querySelector('.img-upload__overlay');
 var buttonClose = upload.querySelector('#upload-cancel');
@@ -35,3 +34,30 @@ uploadButton.addEventListener('change', function (evt) {
 buttonClose.addEventListener('click', function () {
   popupClose();
 });
+
+// var effects = document.querySelectorAll('.effects__radio');
+var pin = document.querySelector('.effect-level__pin');
+// var effectLevel = document.querySelector('.effect-level__value').value / 100;
+// var previewImage = document.querySelector('.img-upload__preview').querySelector('img');
+
+// console.log(pin.offsetLeft);
+
+pin.addEventListener('mouseup', function (evt) {
+  console.log(evt.clientX);
+});
+
+var parent = document.querySelector('.effect-level__line');
+console.log(parent.getBoundingClientRect());
+
+// // for (const effect of effects) {
+// //   effect.addEventListener('change', function () {
+// //     previewImage.removeAttribute('class');
+// //     previewImage.classList.add('effects__preview-' + effect.value);
+// //   });
+// // }
+
+// // for (var i=0; i>length; i++) {
+// //   if previewImage.classList.contains(class[0]) {
+// //     previewImage.styles.filter = filter[0] * effectLevel;
+// //   }
+// }
