57998a14bd0b742bcba31d1343c6523fb0ca8d23
Эльмира <capricorn_26@mail.ru>
2020-06-23 23:26:02 +0700

    Добавляет применение эффектов для изображений (2 часть)

--- js/upload.js
+++ js/upload.js
@@ -40,46 +40,56 @@ buttonClose.addEventListener('click', function () {
 var effectFieldset = document.querySelector('.effect-level');
 var effectPin = effectFieldset.querySelector('.effect-level__pin');
 var effectLine = effectFieldset.querySelector('.effect-level__line');
-var effectLevel = effectFieldset.querySelector('.effect-level__value').value;
+var effectLevel;
 var effectsButtons = document.querySelectorAll('.effects__radio');
 var previewImage = document.querySelector('.img-upload__preview').querySelector('img');
 
-var getEffectLevel = function () {
-  effectLevel = Math.round((effectPin.getBoundingClientRect().left - effectLine.getBoundingClientRect().left));
-};
+// var getEffectLevel = function () {
+//   effectLevel = Math.round((effectPin.getBoundingClientRect().left - effectLine.getBoundingClientRect().left));
+// };
+var getEffectsArray = function (value) {
+  var effects = [
+    {
+      imageClass: 'effects__preview-chrome',
+      filter: 'grayscale(' + 1 * (value / EFFECT_LINE_WIDTH) + ')'
+    },
+    {
+      imageClass: 'effects__preview-sepia',
+      filter: 'sepia(' + 1 * (value / EFFECT_LINE_WIDTH) + ')'
+    },
+    {
+      imageClass: 'effects__preview-marvin',
+      filter: 'invert(' + 100 * (value / EFFECT_LINE_WIDTH) + '%)'
+    },
+    {
+      imageClass: 'effects__preview-phobos',
+      filter: 'blur(' + 3 * (value / EFFECT_LINE_WIDTH) + 'px)'
+    },
+    {
+      imageClass: 'effects__preview-heat',
+      filter: 'brightness(' + (2 * (value / EFFECT_LINE_WIDTH) + 1) + ')'
+    },
+    {
+      imageClass: 'effects__preview-none',
+      filter: 'none'
+    }
+  ];
+  return effects;
+}
 
-var effects = [
-  {
-    imageClass: 'effects__preview-chrome',
-    filter: 'grayscale(' + 1 * (effectLevel / EFFECT_LINE_WIDTH) + ')'
-  },
-  {
-    imageClass: 'effects__preview-sepia',
-    filter: 'sepia(' + 1 * (effectLevel / EFFECT_LINE_WIDTH) + ')'
-  },
-  {
-    imageClass: 'effects__preview-marvin',
-    filter: 'invert(' + 100 * (effectLevel / EFFECT_LINE_WIDTH) + '%)'
-  },
-  {
-    imageClass: 'effects__preview-phobos',
-    filter: 'blur(' + 3 * (effectLevel / EFFECT_LINE_WIDTH) + 'px)'
-  },
-  {
-    imageClass: 'effects__preview-heat',
-    filter: 'brightness(' + 2 * (effectLevel / EFFECT_LINE_WIDTH) + 1 + ')'
-  },
-  {
-    imageClass: 'effects__preview-none',
-    filter: 'none'
-  }
-];
-
-for (let effect of effectsButtons) {
-  effect.addEventListener('change', function () {
-    previewImage.style.filter = 'none';
+var currentEffect;
+effectFieldset.classList.add('hidden');
+for (var effect of effectsButtons) {
+  effect.addEventListener('change', function (evt) {
     previewImage.removeAttribute('class');
-    previewImage.classList.add('effects__preview-' + effect.value);
+    effectLevel = effectFieldset.querySelector('.effect-level__value').value;
+    var effectClassName = 'effects__preview-' + evt.target.value;
+    var currentEffect = getEffectsArray(effectLevel).find(function(effectItem) {
+      return effectItem.imageClass === effectClassName;
+    })
+    previewImage.style.filter = currentEffect.filter;
+
+    previewImage.classList.add(effectClassName);
     if (previewImage.classList.contains('effects__preview-none')) {
       effectFieldset.classList.add('hidden');
     } else {
@@ -89,10 +99,10 @@ for (let effect of effectsButtons) {
 }
 
 effectPin.addEventListener('mouseup', function () {
-  // getEffectLevel();
-  for (var i = 0; i > effects.length; i++) {
-    if (previewImage.classList.contains(effects[i].imageClass)) {
-      previewImage.style.filter = effects[i].filter;
-    }
-  }
+  effectLevel = Math.round((effectPin.getBoundingClientRect().left - effectLine.getBoundingClientRect().left));
+  currentEffect = getEffectsArray(effectLevel).find(function(effectItem) {
+    return effectItem.imageClass === previewImage.className;
+  })
+  previewImage.style.filter = currentEffect.filter;
+
 });
